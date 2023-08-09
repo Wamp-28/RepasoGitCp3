7314debfe168538d529f2caf648ac4f7e4a40870
William Alexander Matallana <wmatallana28@gmail.com>
2023-08-09 18:46:30 -0500

    Salidas

--- src/main/kotlin/Principal.kt
+++ src/main/kotlin/Principal.kt
@@ -1,10 +1,16 @@
 fun main() {
 
     println("Hola Mundo Kotlin")
-
+    println()
+    println()
+    println()
+    println()
     println("Segunda por consola desde kotlin")
 
-
+    println()
+    println()
+    println()
+    println()
 
     println("Mas configuraciones")
     println("Muchas mas configuraciones")
